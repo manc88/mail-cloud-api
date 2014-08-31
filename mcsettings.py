cbe79512a4c3b4f4e20e20ed37b742603f13c278
manc88 <naramanc@gmail.com>
2014-08-31 20:43:49 +0400

    main file, settiings file added

---
+++ mcsettings.py
@@ -0,0 +1,13 @@
+
+
+AUTH = "https://auth.mail.ru/cgi-bin/auth?"
+TOKEN = "https://cloud.mail.ru/api/v1/tokens?"
+DISPATCHER = "https://dispatcher.cloud.mail.ru/u"
+ADDFILE = "https://cloud.mail.ru/api/v1/file/add"
+ADDFOLDER = "https://cloud.mail.ru/api/v1/folder/add"
+LOGOUT = "https://auth.mail.ru/cgi-bin/logout"
+SHARE = "https://cloud.mail.ru/api/v1/file/share"
+UNSHARE = "https://cloud.mail.ru/api/v1/file/unshare"
+REMOVE = "https://cloud.mail.ru/api/v1/file/remove"
+MOVE = "https://cloud.mail.ru/api/v1/file/move"
+RENAME = "https://cloud.mail.ru/api/v1/file/rename"
